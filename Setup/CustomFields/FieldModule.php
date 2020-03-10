43aac2ca1cf47c10d18e63f86db6381a4504fc29
Steffen Rüther <steffen@21st.digital>
2020-03-10 14:29:03 +0100

    - improved FieldModule

--- Setup/CustomFields/FieldModule.php
+++ Setup/CustomFields/FieldModule.php
@@ -7,6 +7,8 @@ class FieldModule
     private $data = [];
     public $name;
 
+    public $attributes = [];
+
     public function __construct($data)
     {
         $this->data = array_merge($this->data, $data);
@@ -17,6 +19,85 @@ class FieldModule
     {
         $res = $this->data;
         $res['name'] = $this->name;
+
+        foreach ($this->attributes as $attribute) {
+            if ($this->isAttribute($attribute)) {
+                $value = call_user_func_array([$this, $this->funcNameForAttribute($attribute)], [$this->get($attribute)]);
+                $res[$attribute] = $value;
+            }
+        }
         return $res;
+    }
+
+    private function isAttribute($name)
+    {
+        return in_array($name, $this->getAttributes())
+            && method_exists($this, $this->funcNameForAttribute($name));
+    }
+
+    private function getAttributes()
+    {
+        return array_merge($this->attributes, ['name']);
+    }
+
+    private function funcNameForAttribute($attribute)
+    {
+        $func = 'get' . array_reduce(explode("_", $attribute), function ($carry, $item) {
+            $carry .= ucfirst($item);
+            return $carry;
+        }, '');
+        dlog($func);
+        return $func;
+    }
+
+    public function __get($attribute)
+    {
+        if (in_array($attribute, $this->getAttributes())) {
+            $func = $this->funcNameForAttribute($attribute);
+            if (method_exists($this, $func)) {
+                return call_user_func_array([$this, $func], [$this->get($attribute)]);
+            }
+        }
+    }
+
+
+    // -----------------------------------------------------
+    // GETTERS & SETTERS
+    // -----------------------------------------------------
+    /**
+     * Get property of model or $default
+     *
+     * @param  property $attribute [description]
+     * @param  property $default
+     * @return mixed
+     *
+     * @todo  investagte this method
+     */
+    public function get($attribute, $default = null)
+    {
+        switch ($attribute) {
+            case 'name':
+                return $this->name;
+            default:
+                if (isset($this->data[$attribute])) {
+                    return $this->data[$attribute];
+                } else {
+                    return $default;
+                }
+        }
+    }
+
+    /**
+     * Set propert of the model
+     *
+     * @param string $attribute
+     * @param string $value
+     * @return void
+     */
+    public function set($attribute, $value)
+    {
+        if (in_array($attribute, $this->getAttributes())) {
+            $this->data[$attribute] = $value;
+        }
     }
 }
