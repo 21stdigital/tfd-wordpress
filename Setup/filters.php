f3232271cb3a4e2da327fedc36e22f45f3555978
Steffen Rüther <steffen@21st.digital>
2020-03-11 16:26:01 +0100

    - fixe hide editor filter issue

--- Setup/filters.php
+++ Setup/filters.php
@@ -13,7 +13,6 @@ add_filter('the_content', function ($content) {
 
 add_filter('acf_the_content', function ($content) {
     if (apply_filters('tfd_remove_wysiwyg_wrapper_tags', true)) {
-        dlog("acf_the_content IN", $content);
         $content = preg_replace('/<p>\s*(<a .*>)?\s*(<img .* \/>)\s*(<\/a>)?\s*<\/p>/iU', '\1\2\3', $content);
         $content = preg_replace('/<p>\s*(<script.*>*.<\/script>)\s*<\/p>/iU', '\1', $content);
         $content = preg_replace('/<p>\s*(<iframe.*>*.<\/iframe>)\s*<\/p>/iU', '\1', $content);
@@ -23,10 +22,15 @@ add_filter('acf_the_content', function ($content) {
 
 // Hide default WordPress editor
 add_action('admin_init', function () {
-    $post_id = $_GET['post'] ? $_GET['post'] : $_POST['post_ID'] ;
-    if (!isset($post_id)) {
+    if (isset($_GET['post'])) {
+        $post_id = $_GET['post'];
+    } elseif (isset($_POST['post_ID'])) {
+        $post_id = $_POST['post_ID'];
+    }
+    if (!isset($post_id) || empty($post_id)) {
         return;
     }
+
     $template_file = get_post_meta($post_id, '_wp_page_template', true);
     $templates = apply_filters('tfd_hide_default_editor_templates', []);
     foreach ($templates as $template) {
