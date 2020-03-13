ea460bcd20b43d00ed2ca52c57c93656ea2ab33b
Steffen Rüther <steffen@21st.digital>
2020-03-13 10:22:54 +0100

    - added helper function array_keys_exists()

---
+++ Setup/helpers.php
@@ -0,0 +1,9 @@
+<?php
+
+
+if (!function_exists('array_keys_exists')) {
+    function array_keys_exists(array $keys, array $arr)
+    {
+        return !array_diff_key(array_flip($keys), $arr);
+    }
+}
